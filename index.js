2d66fcc4623edcaf56bb8d46f025975a037e889c
Jesse <jharlan628@gmail.com>
2021-09-27 10:37:47 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+ fetch('https://anapioficeandfire.com/api/books')
+ .then(res => res.json())
+ .then(portrayData => renderBooks(portrayData))
+ return fetch('https://anapioficeandfire.com/api/books')
+}
+
+  function renderBooks(books) {
+    const main = document.querySelector('main');
+    books.forEach(book => {
+      const h2 = document.createElement('h2');
+      h2.innerHTML = book.name;
+      main.appendChild(h2);
+    });
+  }
   
-}
-
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+  document.addEventListener('DOMContentLoaded', function() {
+    fetchBooks();
   });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
